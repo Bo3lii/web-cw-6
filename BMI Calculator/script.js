2f3ff21f22b8966a1008bf6db2289e4f524bcaf9
Bo3lii <aoosa.alhaddad1@gmail.com>
2022-03-20 20:23:37 +0300

    day6

--- BMI Calculator/script.js
+++ BMI Calculator/script.js
@@ -1,1 +1,48 @@
 // هنا سوف تنشئ دالة calculate 
+function calculate(){
+    let weight = document.getElementById("weight").value;
+    let height = document.getElementById("height").value;
+
+
+    let BMi = weight/(height*height);
+
+    let status = "";
+
+
+  if(BMi<=18.5){
+      status="انت حدك عصل وايد ضعيف شد حيلك "}
+      else if(BMi<=25){
+          status="وزنك بيرفكت تبارك الله استمر"
+      }else if(BMi<=100){
+        status="وزنك اوفر شوف لك حل او اي دبرة"
+      }else{
+          alert(" الله يسامحك")
+          status="error"
+      }
+
+  if(weight ==""){
+    alert("اكتب وزنك لا تستحي")
+}else if(height==""){
+    alert("اكتب طولك لا اصيدك")}
+
+// }else if(height=="" && weight=="");{
+//     alert("تستعبط؟ اكتب طولك و وزنك لا اصيدك")
+// }
+
+if(BMi<=18.5){
+    status="انت حدك عصل وايد ضعيف شد حيلك "}
+    else if(BMi<=25){
+        status="وزنك بيرفكت تبارك الله استمر"
+    }else if(BMi<=100){
+      status="وزنك اوفر شوف لك حل او اي دبرة"
+    }else{
+        alert(" الله يسامحك")
+        status="error"
+    }
+
+BMi=BMi.toFixed(2);
+  document.getElementById("result").innerHTML=BMi;
+  document.getElementsByClassName("comment")[0].innerHTML=status
+}
+
+
---
+++ Exercise 1/ex1.js
@@ -0,0 +1,11 @@
+function travel(u="earth"){
+
+console.log(" abe asafr to " + u);
+}
+ 
+// let planet = prompt("اكتب اسم الكوكب")
+let planet
+travel(planet);
+
+
+
